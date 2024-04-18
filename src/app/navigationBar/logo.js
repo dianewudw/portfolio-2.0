dddef711f1ae434e57ac1e400253e7c7ad58a606
dianewudw <diane@Dianes-Air.localdomain>
2024-04-18 11:33:45 -0500

    updated hover color

--- src/app/navigationBar/logo.js
+++ src/app/navigationBar/logo.js
@@ -13,7 +13,7 @@ export default function Logo({ MobileMenuButton}){
         <>
             <div className="flex w-full justify-between items-center content-center font-header">
                 <div className="lg:flex w-full flex-nowrap inline-flex items-center no-underline max-h-full ml-12 lg:ml-0">
-                    <a href='/ ' className={`${saunde.className} text-4xl lg:text-5xl w-full text-white text-center lg:text-start`}>Diane Wu</a>
+                    <a href='/ ' className={`${saunde.className} text-4xl lg:text-5xl w-full text-white text-center lg:text-start hover:text-button`}>Diane Wu</a>
                     <Image src='Open.svg' alt='hamburger_icon' height={48} width={48} onClick={MobileMenuButton} className='lg:hidden'/>
                 </div>
             </div>
