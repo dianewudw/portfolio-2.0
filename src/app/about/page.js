aa870f493046e464a9164a9ea12569563aa657e1
dianewudw <diane@Dianes-Air.localdomain>
2024-04-19 14:04:52 -0500

    updated contact me page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -6,14 +6,14 @@ import { abrilFatface, poppins } from "@/app/fonts";
 export default function About(){
     return(
         <>
-            <div class='flex flex-col h-lvh w-full p-5 lg:p-24 text-white '>
+            <div class='flex flex-col h-lvh w-full p-5 lg:px-24 lg:pt-24 text-white '>
                 <NavigationBar/>
                 <h1 className={`${abrilFatface.className} flex self-center font-display lg:my-20 mt-10 mb-5 text-4xl lg:text-6xl `}>About Me</h1>
                 <div className='flex flex-col md:flex-row justify-center md:items-center w-full rounded-lg bg-white/5 py-10 h-svh overflow-scroll'>
-                    <div className='flex relative w-full h-3/5 lg:h-full lg:w-1/2 lg:self-center justify-center align-center' >
+                    <div className='flex relative w-full h-full lg:w-1/2 lg:self-center justify-center align-center' >
                         <Image src='/memoji.png' alt='Prof Pic' fill={true} class='flex object-contain rounded-full absolute'/>
                     </div>
-                    <div className={` ${poppins.className} flex flex-col text-lg md:text-xl min-h-fit md:max-h-full lg:w-1/2 lg:justify-items-center text-center lg:text-start overflow-scroll`}>
+                    <div className={` ${poppins.className} flex flex-col text-lg md:text-xl min-h-fit md:max-h-full lg:w-1/2 lg:justify-items-center text-center lg:text-start`}>
                         <p class='flex pt-2 px-4 lg:p-5'>
                             {`Diane is a product designer and developer from Austin, Texas. Her interest in building cool stuff comes from a love of aesthetics without sacrificing function. This led her to begin a career in developing products, from developing haircare products for cosmetic giants to building microservices for unicorn start-ups.`}
                         </p>
@@ -25,17 +25,17 @@ export default function About(){
                         </p>
                     </div>
                 </div>
-            </div>
-            <div className='flex flex-row justify-end my-2 lg:h-1/6 mt-5 mb-5 px-5 lg:px-24 pb:5'>
-                <div className='flex justify-center self-center px-5'>
-                    <a href='https://linkedin.com/in/dianewudw' >
-                        <Image src='/linkedin_dark_mode.svg' alt='linkedin icon' className='object-contains' height={40} width={40}/>
-                    </a>
-                </div>
-                <div className='flex justify-center self-center px-5'>
-                    <a href='https://github.com/dianewudw'>
-                        <Image src='/github_dark_mode.svg' alt='github icon' className='object-contains' height={40} width={40}/>
-                    </a>
+                <div className='flex flex-row justify-end lg:h-1/6 mt-5 px-5 lg:pb:5'>
+                    <div className='flex justify-center self-center px-5'>
+                        <a href='https://linkedin.com/in/dianewudw' >
+                            <Image src='/linkedin_dark_mode.svg' alt='linkedin icon' className='object-contains' height={40} width={40}/>
+                        </a>
+                    </div>
+                    <div className='flex justify-center self-center px-5'>
+                        <a href='https://github.com/dianewudw'>
+                            <Image src='/github_dark_mode.svg' alt='github icon' className='object-contains' height={40} width={40}/>
+                        </a>
+                    </div>
                 </div>
             </div>
         </>
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -13,10 +13,10 @@ export default function Contact(){
 
     return(
         <>
-            <div className='flex flex-col h-full w-full p-5 lg:p-24 text-white'>
+            <div className='flex flex-col h-full w-full p-5 lg:px-24 lg:pt-24 text-white'>
                 <NavigationBar />
-                <div class={` ${poppins.className} flex lg:mt-30 h-lvh lg:h-svh justify-center w-full flex-col sm:flex-row`}>
-                    <div class='flex w-full flex-col h-full lg:h-4/5 lg:flex-row justify-center bg-white/[0.05] my-10 lg:my-20 rounded-lg py-10 rounded'>
+                <div class={` ${poppins.className} flex lg:mt-30 h-lvh lg:h-svh justify-center w-full flex-col flex-row`}>
+                    <div class='flex w-full flex-col h-full lg:h-4/5 lg:flex-row justify-center bg-white/[0.05] my-10 lg:my-20 rounded-lg py-10'>
                         <div className='relative flex h-full lg:w-1/2 w-full'>
                             <Image src='/Contact_page.png' alt='placeholder' fill={true} class='w-1/2 px-10 object-scale-down absolute' />
                         </div>
@@ -36,17 +36,17 @@ export default function Contact(){
                             <a href='/submit' className='bg-button/80 py-2 rounded-full w-full mt-3 text-center my-5 hover:bg-white/20 hover:border hover:border-white/20'>Send</a>
                         </form>
                     </div>
-                </div>
-                <div className='flex flex-row justify-end my-2 lg:h-1/6'>
-                    <div className='flex justify-center self-center px-5'>
-                        <a href='https://linkedin.com/in/dianewudw' >
-                            <Image src='/linkedin_dark_mode.svg' alt='linkedin icon' className='object-contains' height={40} width={40}/>
-                        </a>
-                    </div>
-                    <div className='flex justify-center self-center px-5'>
-                        <a href='https://github.com/dianewudw'>
-                            <Image src='/github_dark_mode.svg' alt='github icon' className='object-contains' height={40} width={40}/>
-                        </a>
+                    <div className='flex flex-row justify-end my-2 lg:h-1/6 '>
+                        <div className='flex justify-center self-center px-5'>
+                            <a href='https://linkedin.com/in/dianewudw' >
+                                <Image src='/linkedin_dark_mode.svg' alt='linkedin icon' className='object-contains' height={40} width={40}/>
+                            </a>
+                        </div>
+                        <div className='flex justify-center self-center px-5'>
+                            <a href='https://github.com/dianewudw'>
+                                <Image src='/github_dark_mode.svg' alt='github icon' className='object-contains' height={40} width={40}/>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
